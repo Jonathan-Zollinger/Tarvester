831773ab92f2ed669eb179a595b85c6979a7e980
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-11-30 20:04:45 -0700

    refactor: remove semicolons

--- src/main/kotlin/com/graqr/threshr/model/redsky/store/Geofence.kt
+++ src/main/kotlin/com/graqr/threshr/model/redsky/store/Geofence.kt
@@ -1,6 +1,6 @@
-package com.graqr.threshr.model.redsky.store;
+package com.graqr.threshr.model.redsky.store
 
-import io.micronaut.serde.annotation.Serdeable;
+import io.micronaut.serde.annotation.Serdeable
 
 @Serdeable
 data class Geofence(val latitude: Double, val longitude: Double, val radius: Long) {}
